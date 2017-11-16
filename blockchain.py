452dc8c79faa524cf8fd7c99bd0d0dd5f72be791
student <student@studentnoMacBook-Pro.local>
2017-11-16 18:03:05 +0900

    add replace function

--- blockchain.py
+++ blockchain.py
@@ -2,45 +2,55 @@ import hashlib
 import datetime
 
 class BlockChain():
+    def __init__(self):
+        self.blocks = []
+        self.latest_index=-1
+        #ジェネシスブロックを追加
+        self.add(self.get_genesis_block())
 
-	def __init__(self):
-		self.blocks = []
-		self.latest_index=-1
-		#ジェネシスブロックを追加
-		self.add(self.get_genesis_block())
-		
-	def add(self,block=None,data=None):
+    def __len__(self):
+        return len(self.blocks)
 
-		#blockとdataの両方が指定されていない場合
-		if not (block or data):
-			return 
+    def add(self,block=None,data=None):
 
-		#dataが指定されている場合
-		if not block:
-			index=self.latest_index+1
-			previous_hash=self.blocks[-1].hash
-			timestamp=int(datetime.datetime.timestamp(datetime.datetime.now()))
-			hash=Block.calc_hash_from_args(index,previous_hash,timestamp,data)
-			block=Block(index,previous_hash,timestamp,data,hash)
-		
-		# blockが有効ならばblocksに追加
-		if len(self.blocks)==0 or block.is_valid(self.blocks[-1]):
-			self.blocks.append(block)
-			self.latest_index+=1
-		return
+        #blockとdataの両方が指定されていない場合
+        if not (block or data):
+            return
+
+        #dataが指定されている場合
+        if not block:
+            index=self.latest_index+1
+            previous_hash=self.blocks[-1].hash
+            timestamp=int(datetime.datetime.timestamp(datetime.datetime.now()))
+            hash=Block.calc_hash_from_args(index,previous_hash,timestamp,data)
+            block=Block(index,previous_hash,timestamp,data,hash)
+
+        # blockが有効ならばblocksに追加
+        if len(self.blocks)==0 or block.is_valid(self.blocks[-1]):
+            self.blocks.append(block)
+            self.latest_index+=1
+        return
+
+    def get_genesis_block(self):
+        return Block(0,
+            "0",
+            1465154705,
+            "my genesis block!!",
+            "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"
+        )
 
 
+    def is_valid(self):
+        return True
+    def replace(self,blockchain):
+        if blockchain.is_valid() and len(blockchain) > len(self):
+            self.blocks=blockchain
+            self.latest_index=blockchain[-1].index
 
-	def get_genesis_block(self):
-		return Block	(0, 
-				"0",
-				 1465154705,
-				 "my genesis block!!",
-				 "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"
-				)
+
 
 class Block():
-	
+
 	def __init__(self,index,previous_hash,timestamp,data,hash):
 		self.index=index
 		self.previous_hash=previous_hash
@@ -58,7 +68,7 @@ class Block():
 			print("calc_hash")
 			return False
 		return True
-		
+
 	def calc_hash(self):
 
 		return hashlib.sha256((str(self.index)+self.previous_hash+str(self.timestamp)+str(self.data)).encode('utf-8')).hexdigest()
